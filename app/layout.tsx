fb1b1ea775e73ab478a4b4ad045e51a2a8d4a695
UnknownCode-2026 <anuthit17@gmail.com>
2026-09-26 15:14:05 +0700

    chore: align Krobkrueng V1.4 browser theme color

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  themeColor: "#006b32",
+  themeColor: "#0b8f4f",
 };
 
 export const metadata: Metadata = {
